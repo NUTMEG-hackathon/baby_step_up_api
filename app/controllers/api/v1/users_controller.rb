cd95dd6ac63cfe6a3990f255390e08f521eaf5d4
mizu-ryo <mukusuke421@gmail.com>
2021-07-04 08:46:50 +0900

    [mod]update_select_template(#24)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,12 @@ class Api::V1::UsersController < ApplicationController
     render json: { status: 'SUCCESS', message: 'Loaded posts', data: users }
   end
 
+  def update_select_template
+    @user = User.find(update_select_template_params[:user_id])
+    @user.select_template = update_select_template_params[:select_template]
+    @user.save!
+  end
+
   def update_reset_time
     @user = User.find(update_reset_time_params[:user_id])
     @user.reset_time = update_reset_time_params[:reset_time]
@@ -12,8 +18,13 @@ class Api::V1::UsersController < ApplicationController
 
   private
   
+    def update_select_template_params
+      params.permit(:user_id, :select_template)
+    end
+
     def update_reset_time_params
       params.permit(:user_id, :reset_time)
     end
+
 end
 
